1e1b43b101084d5ff5348bd7ea8792797f87e328
F.Toutounchi <f.tutunchi@gmail.com>
2023-08-27 14:18:55 +0200

    master first commit

---
+++ src/test/java/com/fto/software/JspProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fto.software;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JspProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
