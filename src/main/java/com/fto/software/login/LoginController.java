b48e5ff12c4292a4392c78233ca03bf69bbd07bf
F.Toutounchi <f.tutunchi@gmail.com>
2023-08-27 20:32:37 +0200

    JSP Get Method

---
+++ src/main/java/com/fto/software/login/LoginController.java
@@ -0,0 +1,26 @@
+package com.fto.software.login;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class LoginController {
+
+
+    @RequestMapping("login")
+    public String getWelcomePage(){
+        return "login";
+    }
+    @RequestMapping("welcome")
+    @ResponseBody
+    public String getLoggedPage(@RequestParam(name = "name") String name,
+                                 @RequestParam(name = "lname") String lname){
+        return "logged by " + name + " " + lname;
+    }
+
+}
---
+++ src/main/resources/META-INF/resources/WEB-INF/jsp/login.jsp
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Simple JSP</title>
+</head>
+<body>
+    <form method="get" action="http://localhost:8080/welcome">
+        name:<input name="name" type="text"/>
+        lname:<input name="lname" type="text"/>
+        <input name="submit" type="submit">
+    </form>
+</body>
+</html>
